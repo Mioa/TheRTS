0a111a0f21b5d6cd0e83f414ddce911acfce28a8
Mioa <Mioa@users.noreply.github.com>
2017-06-14 13:21:25 +0200

    Wat

---
+++ TheRTS/TheRTS/Source/EntityManager.cpp
@@ -0,0 +1,36 @@
+#include "EntityManager.h"
+
+HRESULT EntityManager::Initialize()
+{
+
+}
+
+void EntityManager::Release()
+{
+
+}
+
+EntityManager::EntityManager()
+{
+
+}
+
+EntityManager::~EntityManager()
+{
+
+}
+
+int EntityManager::AddEntity()
+{
+
+}
+
+HRESULT EntityManager::AddComponent( UINT entityIndex, CI_Position info )
+{
+
+}
+
+HRESULT EntityManager::AddComponent( UINT entityIndex, CI_Mesh info )
+{
+
+}
